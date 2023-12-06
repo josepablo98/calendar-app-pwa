17dc9086fcfd70c3fafa54456ecaadeed4f82d1e
Jose Pablo <josepabloruizperez99@gmail.com>
2023-12-06 19:20:38 +0100

    deploy docs

--- docs/sw.js
+++ docs/sw.js
@@ -2,7 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox
 
 workbox.loadModule('workbox-background-sync');
 
-workbox.precaching.precacheAndRoute( self.__WB_MANIFEST );
+workbox.precaching.precacheAndRoute( [{"revision":"a4bcd540f299e548ced7a6b3b1d1267e","url":"asset-manifest.json"},{"revision":"6e1267d9d946b0236cdf6ffd02890894","url":"favicon.ico"},{"revision":"e068b32274d3a0077db58f9c37512a39","url":"index.html"},{"revision":"33dbdd0177549353eeeb785d02c294af","url":"logo192.png"},{"revision":"917515db74ea8d1aee6a246cfbcc0b45","url":"logo512.png"},{"revision":"12cccbf33b373417f7f7733451787dda","url":"manifest.json"},{"revision":"61c27d2cd39a713f7829422c3d9edcc7","url":"robots.txt"},{"revision":"3ad5f4a0bd4ec88ac3d86d7c313a9ac2","url":"static/css/2.bf12cbd8.chunk.css"},{"revision":"a0819237236a18b1fbfef215edc6fec0","url":"static/css/main.7eefda23.chunk.css"},{"revision":"e1135a4bd28cc8610afcb231d2a87be5","url":"static/js/2.c4462d84.chunk.js"},{"revision":"2783cb611cfe78d22f7194d9d4695716","url":"static/js/2.c4462d84.chunk.js.LICENSE.txt"},{"revision":"89f2d3480cb990c1ef0c9bfb2ebe629b","url":"static/js/main.181348ef.chunk.js"},{"revision":"83017fb57da75be76182ba28f232a3ac","url":"static/js/runtime-main.b56c39d1.js"}] );
 
 const { registerRoute } = workbox.routing;
 const { CacheFirst, NetworkFirst, NetworkOnly } = workbox.strategies;
